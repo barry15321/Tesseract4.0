dd4b34ea84e3f0e10d7e9222b65d1014325fad99
ReplacedToy <barry15321@gmail.com>
2020-02-08 22:13:05 +0800

    Tesseract 4.0 source upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tesseract4.0 source file
---
+++ include/leptonica/endianness.h
@@ -0,0 +1,25 @@
+﻿#if !defined (L_BIG_ENDIAN) && !defined (L_LITTLE_ENDIAN)
+# if 0
+#  ifdef __BIG_ENDIAN__
+#   define L_BIG_ENDIAN
+#  else
+#   define L_LITTLE_ENDIAN
+#  endif
+# else
+#  define L_LITTLE_ENDIAN
+# endif
+#endif
+
+/*
+	#if !defined (L_BIG_ENDIAN) && !defined (L_LITTLE_ENDIAN)
+	# if APPLE_UNIVERSAL_BUILD
+	#  ifdef __BIG_ENDIAN__
+	#   define L_BIG_ENDIAN
+	#  else
+	#   define L_LITTLE_ENDIAN
+	#  endif
+	# else
+	# define ENDIANNESS
+	# endif
+	#endif
+*/
